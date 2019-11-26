5c9a81e2c7168de73c2140be5b071b5300dbf9d3
Callum Whyte <hey@callumwhyte.com>
2019-11-26 14:21:38 +0000

    GetCropUrl extension method taking an enum for crop alias

---
+++ src/Our.Umbraco.Extensions.ImageCropper/Attributes/CropAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Our.Umbraco.Extensions.ImageCropper.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class CropAttribute : Attribute
+    {
+        public string Alias { get; set; }
+
+        public int? Width { get; set; }
+
+        public int? Height { get; set; }
+
+        public CropAttribute(string alias, int? width = null, int? height = null)
+        {
+            Alias = alias;
+            Width = width;
+            Height = height;
+        }
+    }
+}
---
+++ src/Our.Umbraco.Extensions.ImageCropper/Extensions/ImageCropperExtensions.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using Our.Umbraco.Extensions.ImageCropper.Attributes;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Constants = Umbraco.Core.Constants;
+
+namespace Our.Umbraco.Extensions.ImageCropper.Extensions
+{
+    public static class ImageCropperExtensions
+    {
+        public static string GetCropUrl<T>(this IPublishedContent mediaItem, T cropAlias)
+            where T : struct
+        {
+            return mediaItem.GetCropUrl(cropAlias: cropAlias, useCropDimensions: true);
+        }
+
+        public static string GetCropUrl<T>(this IPublishedContent mediaItem, T cropAlias, string propertyAlias)
+            where T : struct
+        {
+            return mediaItem.GetCropUrl(propertyAlias: propertyAlias, cropAlias: cropAlias, useCropDimensions: true);
+        }
+
+        public static string GetCropUrl<T>(this IPublishedContent mediaItem, T cropAlias, int? width = null, int? height = null, string propertyAlias = Constants.Conventions.Media.File, int? quality = null, ImageCropMode? imageCropMode = null, ImageCropAnchor? imageCropAnchor = null, bool preferFocalPoint = false, bool useCropDimensions = false, bool cacheBuster = true, string furtherOptions = null, ImageCropRatioMode? ratioMode = null, bool upScale = true)
+            where T : struct
+        {
+            var crop = GetCropAttribute(cropAlias);
+
+            var alias = string.IsNullOrWhiteSpace(crop.Alias) == false ? crop.Alias : null;
+
+            if (width.HasValue == false)
+            {
+                width = crop.Width;
+            }
+
+            if (height.HasValue == false)
+            {
+                height = crop.Height;
+            }
+
+            return mediaItem.GetCropUrl(width, height, propertyAlias, alias, quality, imageCropMode, imageCropAnchor, preferFocalPoint, useCropDimensions, cacheBuster, furtherOptions, ratioMode, upScale);
+        }
+
+        private static CropAttribute GetCropAttribute<T>(T cropAlias)
+        {
+            var alias = cropAlias.ToString();
+
+            var type = typeof(T);
+
+            var property = type.GetMember(alias);
+
+            var cropAttribute = property[0].GetCustomAttribute<CropAttribute>();
+
+            return cropAttribute;
+        }
+    }
+}
