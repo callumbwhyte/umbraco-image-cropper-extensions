9dd5d88fef1ad44f10c0bfc324a17fd3ff412880
Callum Whyte <hey@callumwhyte.com>
2019-11-26 15:28:50 +0000

    Adding URL extension methods to compliment existing Umbraco extensions

---
+++ src/Our.Umbraco.Extensions.ImageCropper/Extensions/UrlExtensions.cs
@@ -0,0 +1,59 @@
+﻿using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using Our.Umbraco.Extensions.ImageCropper.Attributes;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Constants = Umbraco.Core.Constants;
+
+namespace Our.Umbraco.Extensions.ImageCropper.Extensions
+{
+    public static class UrlExtensions
+    {
+        public static IHtmlString GetCropUrl<T>(this UrlHelper urlHelper, IPublishedContent mediaItem, T cropAlias, bool htmlEncode = true)
+            where T : struct
+        {
+            return urlHelper.GetCropUrl(mediaItem, cropAlias, useCropDimensions: true, htmlEncode: htmlEncode);
+        }
+
+        public static IHtmlString GetCropUrl<T>(this UrlHelper urlHelper, IPublishedContent mediaItem, T cropAlias, string propertyAlias, bool htmlEncode = true)
+            where T : struct
+        {
+            return urlHelper.GetCropUrl(mediaItem, cropAlias, propertyAlias: propertyAlias, useCropDimensions: true, htmlEncode: htmlEncode);
+        }
+
+        public static IHtmlString GetCropUrl<T>(this UrlHelper urlHelper, IPublishedContent mediaItem, T cropAlias, int? width = null, int? height = null, string propertyAlias = Constants.Conventions.Media.File, int? quality = null, ImageCropMode? imageCropMode = null, ImageCropAnchor? imageCropAnchor = null, bool preferFocalPoint = false, bool useCropDimensions = false, bool cacheBuster = true, string furtherOptions = null, ImageCropRatioMode? ratioMode = null, bool upScale = true, bool htmlEncode = true)
+            where T : struct
+        {
+            var crop = GetCropAttribute(cropAlias);
+
+            var alias = string.IsNullOrWhiteSpace(crop.Alias) == false ? crop.Alias : null;
+
+            if (width.HasValue == false)
+            {
+                width = crop.Width;
+            }
+
+            if (height.HasValue == false)
+            {
+                height = crop.Height;
+            }
+
+            return urlHelper.GetCropUrl(mediaItem, width, height, propertyAlias, alias, quality, imageCropMode, imageCropAnchor, preferFocalPoint, useCropDimensions, cacheBuster, furtherOptions, ratioMode, upScale, htmlEncode);
+        }
+
+        private static CropAttribute GetCropAttribute<T>(T cropAlias)
+        {
+            var alias = cropAlias.ToString();
+
+            var type = typeof(T);
+
+            var property = type.GetMember(alias);
+
+            var cropAttribute = property[0].GetCustomAttribute<CropAttribute>();
+
+            return cropAttribute;
+        }
+    }
+}
